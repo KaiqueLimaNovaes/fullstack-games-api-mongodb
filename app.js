f516ad9f74c8ac5b8294b2a68ea2ae886a9c45f8
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-11 11:51:28 -0300

    api mongo db

---
+++ app.js
@@ -0,0 +1,59 @@
+const cors = require("cors");
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json())
+
+//models
+require('./src/models/Contato')
+const Contato = mongoose.model('contatos')
+
+//Conexão db
+require('./src/db/connect')
+
+app.get('/contatos', async (req, res) => {
+    const contatosResponse = await Contato.find()
+    const contatosJson = await contatosResponse
+
+    return res.json(contatosJson)
+})
+
+app.post('/contatos', async (req, res) => {
+    const novoContato = new Contato({
+        nome: req.body.nome,
+        email: req.body.email,
+        menssagem: req.body.menssagem
+    })
+
+    novoContato.save()
+
+    res.json({message: "Contato concluido com sucesso", contato:novoContato})
+})
+
+app.put('/contatos/:id', async (req, res) => {
+    const {id} = req.params
+    //pesquisando por um unico contato
+    const contato = await Contato.findOne({_id: id})
+
+    //alterando os dados existentes
+    contato.nome = req.body.nome
+    contato.email = req.body.email
+    contato.menssagem = req.body.menssagem
+
+    //salvando as alterações
+    contato.save()
+
+    res.json({message: "Contato alterado", contato: contato})
+})
+
+app.delete('/contatos/:id', async (req, res) => {
+    const {id} = req.params
+    //pesquisando por um unico contato
+    const contato = await Contato.findOneAndDelete({_id: id})
+
+    res.json({message: "Contato deletado com sucesso", contato: contato})
+})
+
+app.listen(3210);
---
+++ src/db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connect() {
+    mongoose
+        .connect('mongodb://localhost/fullstackgamesmongodb', {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+            console.log('Conectado ao db')
+        }).catch((error) => {
+            console.log(`Não conectado ${error}`)
+        })
+}
+
+module.exports = connect();
